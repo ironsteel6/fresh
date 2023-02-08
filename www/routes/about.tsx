3260deb2673e4bfb09c57cc0cd14f61803852823
ironsteel6 <77941833+ironsteel6@users.noreply.github.com>
2023-02-08 16:46:57 -0600

    Update about.tsx

--- www/routes/about.tsx
+++ www/routes/about.tsx
@@ -8,7 +8,7 @@ export const handler: Handlers = {
     const url = 'https://www.wsj.com/articles/global-stocks-markets-dow-update-12-12-2022-11670845873';
 
     const res = await fetch(url);
-    const body = new Uint8Array(await res.arrayBuffer());
+    //const body = new Uint8Array(await res.arrayBuffer());
     
     //return new Response(body));
     return new Response(JSON.stringify(uuid), {
