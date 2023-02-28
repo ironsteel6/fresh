8c2e74cf4caecdaa009ce1093d6888e65b58bf6b
ironsteel6 <77941833+ironsteel6@users.noreply.github.com>
2023-02-28 12:18:17 -0600

    Update ironsteel6.tsx

--- www/routes/ironsteel6.tsx
+++ www/routes/ironsteel6.tsx
@@ -14,7 +14,8 @@ export const handler: Handlers<User | null> = {
     //const resp = await fetch(`https://api.github.com/users/${username}`);
     //const url = 'https://www.wsj.com/articles/global-stocks-markets-dow-update-12-12-2022-11670845873';
     //const url = 'https://www.nytimes.com/2023/01/11/opinion/electric-vehicles-sales-growth-tesla.html';
-    const url = 'https://www.nytimes.com/2022/02/18/well/mind/adhd-dating-relationships.html';
+    //const url = 'https://www.nytimes.com/2022/02/18/well/mind/adhd-dating-relationships.html';
+    const url = 'https://www.chicagotribune.com/politics/elections/ct-chicago-election-voter-turnout-20230228-7v33ulb4r5crxareze3c2ypvmy-story.html';
     const resp = await fetch(url);
     const body = new Uint8Array(await resp.arrayBuffer());
     //await Deno.stdout.write(body);
