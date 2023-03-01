3364b78a38efaefced58e7a5c44e939335799a3a
ironsteel6 <77941833+ironsteel6@users.noreply.github.com>
2023-03-01 16:59:50 -0600

    Update ironsteel5.tsx

--- www/routes/ironsteel5.tsx
+++ www/routes/ironsteel5.tsx
@@ -5,16 +5,16 @@ import { PageProps } from "$fresh/server.ts";
 export default function GreetPage(props: PageProps) {
   const { name } = props.params;
   return (
-    /*
     <main>
       <p>Greetings to you, {name}!</p>
     </main>
-    */
+    /*
     <main>
     <form>
     <label>URL: <input type="text"></label>
     <input type="submit" value="Submit">
     </form>
       </main>
+      */
   );
 }
