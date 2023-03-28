6cc5b64293ab5553b2c33eda08579be8fd10920f
ironsteel6 <77941833+ironsteel6@users.noreply.github.com>
2023-03-28 12:11:14 -0500

    Update ironsteel6.tsx

--- www/routes/ironsteel6.tsx
+++ www/routes/ironsteel6.tsx
@@ -15,7 +15,8 @@ export const handler: Handlers<User | null> = {
     //const url = 'https://www.wsj.com/articles/global-stocks-markets-dow-update-12-12-2022-11670845873';
     //const url = 'https://www.nytimes.com/2023/01/11/opinion/electric-vehicles-sales-growth-tesla.html';
     //const url = 'https://www.nytimes.com/2022/02/18/well/mind/adhd-dating-relationships.html';
-    const url = 'https://www.chicagotribune.com/politics/elections/ct-chicago-election-voter-turnout-20230228-7v33ulb4r5crxareze3c2ypvmy-story.html';
+    //const url = 'https://www.chicagotribune.com/politics/elections/ct-chicago-election-voter-turnout-20230228-7v33ulb4r5crxareze3c2ypvmy-story.html';
+    const url = 'https://www.nytimes.com/2023/03/28/well/live/nap-benefits-sleep-loss.html';
     const resp = await fetch(url);
     const body = new Uint8Array(await resp.arrayBuffer());
     //await Deno.stdout.write(body);
